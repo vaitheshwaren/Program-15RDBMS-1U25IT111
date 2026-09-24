66baf5037919e378bd51e9dcdb23d6d2a81cac3a
VAITHEESWARAN P <vaitheshwaren6@gmail.com>
2026-09-24 13:40:46 +0530

    Program 15

--- starter.sql
+++ starter.sql
@@ -1,23 +1,42 @@
--- PL/SQL Assignment
--- Program: Check whether a student has passed or failed
--- Pass mark: 40
 
 SET SERVEROUTPUT ON;
 
--- Accept marks from the user
-ACCEPT marks NUMBER PROMPT 'Enter student marks: '
-
 DECLARE
-    v_marks NUMBER := &marks;
+    marks NUMBER;
 BEGIN
+    marks := 35;
+    IF marks >= 40 THEN
+        DBMS_OUTPUT.PUT_LINE('PASS');
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('FAIL');
+    END IF;
 
-    -- Write your IF-ELSE statement here
+    marks := 40;
+    IF marks >= 40 THEN
+        DBMS_OUTPUT.PUT_LINE('PASS');
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('FAIL');
+    END IF;
 
-    -- If marks are greater than or equal to 40,
-    -- display: PASS
+    marks := 75;
+    IF marks >= 40 THEN
+        DBMS_OUTPUT.PUT_LINE('PASS');
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('FAIL');
+    END IF;
 
-    -- Otherwise,
-    -- display: FAIL
+    marks := 20;
+    IF marks >= 40 THEN
+        DBMS_OUTPUT.PUT_LINE('PASS');
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('FAIL');
+    END IF;
 
+    marks := 100;
+    IF marks >= 40 THEN
+        DBMS_OUTPUT.PUT_LINE('PASS');
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('FAIL');
+    END IF;
 END;
-/ 
+/
